7b4a92bbf55eaef41b1fefe8e7b5fb7b91370169
micowav <tinyducky1@proton.me>
2026-08-07 10:33:01 +0200

    first files

---
+++ nvim/lua/plugins/custom.lua
@@ -0,0 +1,48 @@
+return {
+  {
+    "olimorris/onedarkpro.nvim",
+    lazy = false,
+    priority = 1000,
+
+    config = function()
+      require("onedarkpro").setup({})
+      vim.cmd([[colo onedark]])
+    end,
+  },
+  {
+    "snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          pick = function(cmd, opts)
+            return LazyVim.pick(cmd, opts)()
+          end,
+          header = [[
+ _______             ____   ____.__         
+ \      \   ____  ___\   \ /   /|__| _____  
+ /   |   \_/ __ \/  _ \   Y   / |  |/     \ 
+/    |    \  ___(  <_> )     /  |  |  Y Y  \
+\____|__  /\___  >____/ \___/   |__|__|_|  /
+        \/     \/                        \/ 
+        ]],
+
+          -- stylua: ignore
+          ---@type snacks.dashboard.Item[]
+          keys = {
+            { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+            { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+            { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+            { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+            { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+            { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+            { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+          },
+        },
+      },
+    },
+  },
+  {
+    "folke/sidekick.nvim",
+  },
+}
